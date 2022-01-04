ff4481b6aadd4f58378c49d2ab87143ac6955950
anna97490 <anna.cousin97490@gmail.com>
2022-01-04 20:25:18 +0100

    init routes folder

---
+++ backend/routes/comment.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+//router.get('/:id',);
+//router.get('/:id',);
+//router.post('/:id',);
+//router.put('/:id',);
+
+
+
+
+
+module.exports = router;
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+//const userCtrl
+//const multer
+
+//router.post('/signup',);
+//router.post('/login',);
+//router.get('/:id');
+//router.put('/updateProfil/:id',);
+//router.put('/updatePicture/:id',);
+//router.put('/updatePassword/:id',);
+
+module.exports = router;
