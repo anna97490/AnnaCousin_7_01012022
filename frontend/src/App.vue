a27efc2278b7e81469892092f70528f048632eb5
anna97490 <anna.cousin97490@gmail.com>
2022-01-12 12:20:15 +0100

    Home Page config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+* {
+  font-family: 'Roboto', sans-serif;
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+  max-width: 100%;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 32px;
+  background-color: #f0f2f5;
 }
+
+
+
+  
 </style>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,10 @@
 import { createStore } from 'vuex'
+const axios = require('axios');
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/api/auth'
+});
+
 
 export default createStore({
   state: {
@@ -6,6 +12,16 @@ export default createStore({
   mutations: {
   },
   actions: {
+    createAccount: ({commit}, userInfos) => {
+     commit;
+     instance.post('/createAccount', userInfos)
+     .then(function (response){
+       console.log(response);
+     })
+     .catch(function (error){
+      console.log(error);
+     });
+    }
   },
   modules: {
   }
