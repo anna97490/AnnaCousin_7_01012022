769d32abbbb6778f38f9ef8360d8e018719ef694
anna97490 <anna.cousin97490@gmail.com>
2022-01-13 23:04:30 +0100

    reupdate home and signup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Posts from '../views/Posts.vue'
+import Signup from '../views/Signup.vue'
 
 Vue.use(VueRouter)
 
@@ -12,9 +12,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/posts',
-    name: 'Posts',
-    component: Posts
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
   }
 ]
 
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -3,49 +3,56 @@
     <div class="card__container">
       <img class="logo" src="../assets/icon-above-font.png">
     </div>
-    <h1 class="card__title" v-if="mode == 'login'">Votre réseau social d'entreprise!</h1>
-    <router-link class="card__signup" to="/signup"><strong>S'enregistrer</strong></router-link>
+    <h1 class="card__title" v-if="mode == 'signup'">Votre réseau social d'entreprise!</h1>
+    <router-link class="card__signup" to="/" width="100%"><strong>Se connecter</strong></router-link>
     <form>
       <div class="card__form">
-        <input type="email" v-model="email" placeholder="Email" required aria-label="Email de connection">
-        <!--<label for="email-adress">Mot de passe</label>-->
-        <input type="password" v-model="password" placeholder="Password" required aria-label="Mot de passe de connection"> 
+        <input class="card__form--input1" type="email" v-model="email" placeholder="Email" required aria-label="Email de connection">
+        <div class="card__form--name">
+          <input  class="card__form--input2"  type="firstname" v-model="firstname" placeholder="Prénom" required aria-label="Prénom pour la connection">
+          <input  class="card__form--input2"  type="lastname" v-model="lastname" placeholder="Nom" required aria-label="Nom pour la connection">
+        </div>
+        <input  class="card__form--input1" type="password" v-model="password" placeholder="Password" required aria-label="Mot de passe de connection"> 
       </div>
     </form>
-    <button class="card__btn" @click="login()" type="submit" aria-label="Se connecter"><strong>Se connecter</strong></button>
+    <button class="card__btn" @click="signup()" type="submit" aria-label="Se connecter"><strong>Se connecter</strong></button>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: 'Login',
+  name: 'Signup',
   data: function () {
     return {
-      mode: 'login',
+      mode: 'signup',
       email: '',
-      password: '',
+      firstname: '',
+      lastname: '',
+      password: ''
     }
   },
   methods: {
-    login: function () {
+    signup: function () {
       let newUser = {
         email: this.email,
+        firstname: this.firstname,
+        lastname: this.lastname,
         password: this.password
       }
-      axios.post("http://localhost:3000/api/users/login", newUser)
+      axios.post("http://localhost:3000/api/users/signup", newUser)
       .then((res) => {
         if (res.status === 201) {
-          localStorage.setItem("logged", JSON.stringify(res.data));
+          localStorage.setItem("signed", JSON.stringify(res.data));
           this.$router.push("/");
         }
       })
       .catch((err) => {
         localStorage.clear();
         if (err.response.status === 401) {
-          this.error = "Connexion au serveur impossible.";
+          this.error = "Identifiants incorrects!";
         } else {
-          this.error = "Vérifiez vos identifiants. ";
+          this.error = "Problème serveur!";
         }
       })
     },
@@ -53,11 +60,11 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .card {
-  height: 500px;
+  height: 575px;
   width: 540px;
-  padding:32px;
+  padding: 32px;
   display: flex;
   justify-content: center;
   align-content: center;
@@ -71,27 +78,46 @@ export default {
   width: 100%;
 }
 
+.card__form--name {
+  display: flex;
+  margin: 16px 0px;
+  gap:16px;
+  flex-wrap: wrap
+}
+
+.card__form--input2  {
+  padding: 12px;
+  border: none;
+    border-radius: 8px;
+    background:#f2f2f2;
+    font-weight: 500;
+    font-size: 16px;
+    flex:1;
+    min-width: 100px;
+    color: black;
+    box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+}
+
 form {
   width: 100%;
 }
 
-.card__form input {
+.card__form--input1 {
   width: 100%;
   box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
 }
 
 .card__title {
   width: 100%;
-  margin: 6px 0 6px 0;
+  margin-bottom: 5px;
   text-align: center;
   font-size: 13px;
 }
 
 .card__signup {
-  width: 100%;
-  margin: 2% 41%;
-  font-size: 17px;
-  text-decoration: none;
+  margin: 2% 25%;
+  text-align: center;
+  font-weight: 500;
   color: #1976D2;
   transform: scale(0.9);
   transition-property: transform;
@@ -107,7 +133,7 @@ form {
 .card__btn {
   width: 100%;
   padding: 16px;
-  margin-top: 15px;
+  margin-top: 25px;
   font-size: 17px;
   border: none;
   border-radius: 8px;
@@ -135,9 +161,8 @@ form {
 }
 
 form input {
-  width: 100%;
   padding: 12px;
-  margin: 12px 0 12px 0;
+  margin: 3px 0 3px 0;
   border: none;
   border-radius: 8px;
   font-weight: 500;
