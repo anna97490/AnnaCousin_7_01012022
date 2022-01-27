b3d3735e16d91486de16c67885fd84627a5f9f3a
anna97490 <anna.cousin97490@gmail.com>
2022-01-27 19:05:47 +0100

    refacto backend

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -5,45 +5,46 @@ const fs = require('fs');
 exports.createPost = (req, res, next) => {
   if (req.file) {
     Post.create({
-        text: req.body.text,
-        date: req.body.date,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-        userId: req.body.userId,
-        authorFullName: req.body.authorFullName,
+      text: req.body.text,
+      date: req.body.date,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+      userId: req.body.userId,
+      authorFullName: req.body.authorFullName,
     })
     .then(() => res.status(201).json({message: 'Post successfully created!'}))
-    .catch( error => res.status(400).json({error}));
-} else {
+    .catch((error) => res.status(400).json({ error: 'Incorrect request!' }));
+  } else {
     Post.create({
-        text: req.body.text,
-        date: req.body.date,
-        userId: req.body.userId,
-        authorFullName: req.body.authorFullName,
+      text: req.body.text,
+      date: req.body.date,
+      userId: req.body.userId,
+      authorFullName: req.body.authorFullName,
     })
     .then(() => res.status(201).json({message: 'Post successfully created!'}))
-    .catch( error => res.status(500).json({error: 'Post pas posté'}));
+    .catch((error) => res.status(400).json({ error: 'Incorrect request!' }));
   }
 }
 
 // Récupérer un post
 exports.getOnePost = (req, res, next) => {
   Post.findOne({ where: { id: req.params.id } })
-    .then((post) => {
-      if(!post) return res.status(404).json({ error: "Post not found!" });
-      res.status(200).json(post);
-    })
-    .catch((error) => res.status(404).json({ error }));
+  .then((post) => {
+    if(!post) return res.status(404).json({ error: "Post not found!" });
+    res.status(200).json(post);
+  })
+  .catch((error) => res.status(500).json({ error: 'Server Error!' }));
 };
 
 // Récupérer tous les posts
 exports.getAllPost = (req, res, next) => {
   Post.findAll({
+    // Afficher les posts dans l'ordre croissant des dates
     order: [
       ['date', 'DESC']
     ]
   })
   .then((posts) => res.status(200).json(posts))
-  .catch((error) => res.status(400).json({ error}));
+  .catch((error) => res.status(400).json({ error: 'Incorrect request!' }));
 };
 
 // Modifier un post
@@ -60,10 +61,10 @@ exports.updatePost = (req, res, next) => {
       return res.status(404).json({ message: 'Post not found!' });
     } 
     Post.update({...postObject}, { where: { id: req.params.id }})
-      .then(() => res.status(200).json({message: 'Post successfully updated !'}))
-      .catch((error) => res.status(500).json({ error: "no" }));
+    .then(() => res.status(200).json({message: 'Post successfully updated!'}))
+    .catch((error) => res.status(400).json({ error: 'Incorrect request!' }));
   })
-  .catch((error) => res.status(500).json({ error: "no1" }));
+  .catch((error) => res.status(500).json({ error: 'Server error!' }));
 };
 
 //Supprimer un post
@@ -74,25 +75,21 @@ exports.deletePost = (req, res, next) => {
       return res.status(404).json({ message: 'User not found!' })
     } else if(post.imageUrl !== null) {
       const filename = post.imageUrl.split('/images/')[1];
-      fs.unlink(`./images/${filename}`, (err) => {
-        if (err) throw err;
-        Post.destroy({ where: { id: req.params.id }})
+      fs.unlink(`./images/${filename}`, (error) => {
+        if (error) throw error;
+      Post.destroy({ where: { id: req.params.id }})
       .then(() => {
         return res.status(200).json({message: 'Post successfully deleted!'});  
       })
-      .catch((error) => res.status(500).json({ error }));
+      .catch((error) => res.status(500).json({ error: 'Server error!' }));
       });
     } else {
       Post.destroy({
         where: { id: req.params.id }
       })
       .then(() => res.status(200).json({ message: 'Post successfully deleted!' }))
-      .catch(error => {
-        res.status(500).json({ error })
-      })
-    }
-    
+      .catch((error) =>  res.status(500).json({ error: 'Server Error!' }))
+    } 
   })    
-  .catch(error => { res.status(500).json({ error });
-  }) 
+  .catch((error) => res.status(500).json({ error: 'Server error!' })) 
 };
--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -6,71 +6,66 @@ require('dotenv').config();
 
 // Inscription
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = {
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: hash,
-        isAdmin: req.body.isAdmin || 0
-      };
-      User.create(user)
-        .then(() => res.status(201).json({ message: 'User created!' }))
-        .catch((error) =>
-          res.status(400).json({ error: 'Email already used!' })
-        );
+  bcrypt.hash(req.body.password, 10)
+  .then((hash) => {
+    const user = {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: hash,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+      isAdmin: req.body.isAdmin || 0
+    };
+    User.create(user)
+    .then(() => res.status(201).json({ message: 'User created!' }))
+    .catch((error) => res.status(400).json({ error: 'Email already used!' }));
     })
-    .catch((error) => res.status(500).json({ error }));
+  .catch((error) => res.status(500).json({ error: 'Server Error!' }));
 };
 
 // Connexion
 exports.login = (req, res, next) => {
   User.findOne({ where: { email: req.body.email } })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: 'User not found!' });
+  .then((user) => {
+    if (!user) {
+      return res.status(401).json({ error: 'User not found!' });
+    }
+    bcrypt.compare(req.body.password, user.password)
+    .then((valid) => {
+      if (!valid) {
+        return res.status(401).json({ error: 'Invalid Password!' });
       }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Invalid Password!' });
-          }
-          res.status(200).json({
-            userId: user.id,
-            isAdmin: user.isAdmin,
-            token: jwt.sign(
-              { userId: user.id, isAdmin: user.isAdmin },
-              process.env.SECRET_TOKEN,
-              {
-                expiresIn: '24h',
-              }
-            ),
-          });
-        })
-        .catch((error) => res.status(500).json({ error: 'no work' }));
+      res.status(200).json({
+        userId: user.id,
+        isAdmin: user.isAdmin,
+        token: jwt.sign(
+          { userId: user.id, isAdmin: user.isAdmin },
+            process.env.SECRET_TOKEN,
+          { expiresIn: '24h',}
+        )
+      });
     })
-    .catch((error) => res.status(500).json({ error: req.body.id }));
+    .catch((error) => res.status(400).json({ error: 'Incorrect request!' }));
+  })
+  .catch((error) => res.status(500).json({ error: 'Server error!' }));
 };
 
 // Récupérer un user
 exports.getOneUser = (req, res, next) => {
   User.findOne({ where: { id: req.params.id } })
-    .then((user) => {
-      if (!user) return res.status(404).json({ error: 'User not found!' });
-      res.status(200).json({
-        userId: user.id,
-        email: user.email,
-        firstname: user.firstname,
-        lastname: user.lastname,
-        isAdmin: user.isAdmin,
-        imageUrl: user.imageUrl,
-        createdAt: user.createdAt,
-      });
-    })
-    .catch((error) => res.status(404).json({ error }));
+  .then((user) => {
+    if (!user) return res.status(404).json({ error: 'User not found!' });
+    res.status(200).json({
+      userId: user.id,
+      email: user.email,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      isAdmin: user.isAdmin,
+      imageUrl: user.imageUrl,
+      createdAt: user.createdAt,
+    });
+  })
+  .catch((error) => res.status(500).json({ error: 'Server Error!' }));
 };
 
 // Modifier le profil
@@ -79,38 +74,41 @@ exports.updateUser = (req, res, next) => {
     imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
   } 
   User.findOne({ where: { id: req.params.id } })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ message: 'User not found!' });
-      }
-      User.update({ ...userObject }, { where: { id: req.params.id } })
-        .then(() =>
-          res.status(200).json({ message: 'Profil successfully updated !' })
-        )
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+  .then((user) => {
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+    User.update({ ...userObject }, { where: { id: req.params.id } })
+    .then(() =>
+      res.status(200).json({ message: 'Profil successfully updated !' })
+      )
+    .catch((error) => res.status(500).json({ error: 'Server Error!' }));
+  })
+  .catch((error) => res.status(500).json({ error: 'Server Error!' }));
 };
 
 // Supprimer le profil
 exports.deleteUser = (req, res, next) => {
-  User.findOne({ where: { id: req.params.id } })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ message: 'User not found!' });
-      } else {
-        User.destroy({ where: { id: req.params.id } })
-          .then(() => {
-            return res
-              .status(200)
-              .json({ message: 'Profil successfully deleted!' });
-          })
-          .catch((error) => res.status(500).json({ error }));
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+  User.findOne({ where: { id: req.params.id }})
+  .then((user) => {
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' })
+    } else if(user.imageUrl !== null) {
+      const filename = user.imageUrl.split('/images/')[1];
+      fs.unlink(`./images/${filename}`, (err) => {
+        if (err) throw err;
+      User.destroy({ where: { id: req.params.id }})
+      .then(() => {
+        return res.status(200).json({ message: 'Post successfully deleted!' });  
+      })
+      .catch((error) => res.status(500).json({ error: 'Server Error!' }));
+      });
+    } else {
+      User.destroy({ where: { id: req.params.id } })
+      .then(() => res.status(200).json({ message: 'Post successfully deleted!' }))
+      .catch(error => res.status(500).json({  error: 'Server Error!' }))
+    }  
+  })    
+  .catch(error => { res.status(500).json({  error: 'Server Error!' });
+  }) 
 };
